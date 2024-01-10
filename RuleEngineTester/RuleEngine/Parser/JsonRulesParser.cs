52d5c0f3b79a4a3da5e4906a8de78e61ea928f4b
ThanosZikas <147274171+ThanosZikas@users.noreply.github.com>
2024-01-10 06:45:34 +0200

    RuleParserBase encapsulates parsing logic for easy extensibility

---
+++ RuleEngineTester/RuleEngine/Parser/JsonRulesParser.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace RuleEngineTester.RuleEngine.Parser;
+public class JsonRuleParser : RuleParserBase
+{
+    public List<IRule> Parse(string fn)
+    {
+        var data = File.ReadAllText(fn);
+        var ruleSet = JsonConvert.DeserializeObject<RuleSet>(data);
+        if (ruleSet != null)
+        {
+
+            return (List<IRule>)ProcessRuleSet(ruleSet);
+        }
+        else
+            return new List<IRule>();
+    }
+
+}
---
+++ RuleEngineTester/RuleEngine/Parser/RuleAction1.cs
@@ -0,0 +1,8 @@
+﻿namespace RuleEngineTester.RuleEngine.Parser;
+
+public class RuleAction
+{
+    public string Type { get; set; }
+    public string Property { get; set; }
+    public object Value { get; set; }
+}
